64dc33554d88b16177420631ec88ee4aeaecdfac
M-Brenda <brenda_manzano@hotmail.com>
2020-04-17 21:59:23 -0300

    Orders y modificación en swagger

--- routes/orders.js
+++ routes/orders.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const seq = require('./database', 'sequelize');
 const middlewares = require('./middlewares');
+
 //Fecha de hoy para el pedido
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //Enero es 0
 var yyyy = today.getFullYear();
 today = yyyy + '/' + mm + '/' + dd;
 
@@ -21,9 +22,15 @@ router.route('/')
         .then((resultados) => {
             //Si es administrador traigo todos los pedidos cargados
             if (resultados[0].rol_id == 1) {
-                seq.query(`SELECT * from  order_header
-                            INNER JOIN order_items ON order_header.order_id = order_items.order_id
-                            INNER JOIN products ON order_items.prod_id = products.prod_id`,
+                seq.query(`SELECT ord.order_id, ord.user_id, ord.payment_id, ord.status_id, ord.order_total, 
+                            item.item_id, item.item_cant, item.item_price,
+                            prod.prod_id, prod.prod_name,
+                            us.address, us.email, us.full_name, us.user_name, us.telephone
+                            FROM order_header AS ord
+                            INNER JOIN order_items AS item ON ord.order_id = item.order_id
+                            INNER JOIN products AS prod ON item.prod_id = prod.prod_id
+                            INNER JOIN users AS us ON ord.user_id=us.user_id
+                            ORDER BY ord.order_id ASC`,
                 { type: seq.QueryTypes.SELECT }
                 )
                 .then((resultados) => {
@@ -36,10 +43,16 @@ router.route('/')
                  
             } else {
                 //Si es usuario traigo sólo los pedidos asociados a su id
-                seq.query(`SELECT * from  order_header
-                            INNER JOIN order_items ON order_header.order_id = order_items.order_id
-                            INNER JOIN products ON order_items.prod_id = products.prod_id
-                            WHERE order_header.user_id = ${id}`,
+                seq.query(`SELECT ord.order_id, ord.user_id, ord.payment_id, ord.status_id, ord.order_total, 
+                            item.item_id, item.item_cant, item.item_price,
+                            prod.prod_id, prod.prod_name,
+                            us.address, us.email, us.full_name, us.user_name, us.telephone
+                            FROM order_header AS ord
+                            INNER JOIN order_items AS item ON ord.order_id = item.order_id
+                            INNER JOIN products AS prod ON item.prod_id = prod.prod_id
+                            INNER JOIN users AS us ON ord.user_id=us.user_id
+                            WHERE ord.user_id = ${id}
+                            ORDER BY ord.order_id ASC`,
                 { type: seq.QueryTypes.SELECT }
                 )
                 .then((resultados) => {
@@ -101,30 +114,99 @@ router.route('/')
         });
         res.send("Pedido agregado con éxito")
     });
-})
-
-router.route('/:order_id/items')
-    .get(middlewares.autentication, (req, res) => {
-        //res.send("Redireccionado correctamente")
     })
-    .post(middlewares.autentication, (req, res) => {
-        var order = req.params.order_id;
+
+
+router.route('/:order_id')
+    .get(middlewares.autentication, (req, res) => {
+        var order_id = req.params.order_id;
+        var id = req.user.id;
+        seq.query(`SELECT * FROM users WHERE user_id=${id}`,
+            { type: seq.QueryTypes.SELECT }
+            )
+            .then((resultados) => {
+                //Si es administrador traigo el pedido con el id ingesado
+                if (resultados[0].rol_id == 1) {
+                    seq.query(`SELECT ord.order_id, ord.user_id, ord.payment_id, ord.status_id, ord.order_total, 
+                                item.item_id, item.item_cant, item.item_price,
+                                prod.prod_id, prod.prod_name,
+                                us.address, us.email, us.full_name, us.user_name, us.telephone
+                                FROM order_header AS ord
+                                INNER JOIN order_items AS item ON ord.order_id = item.order_id
+                                INNER JOIN products AS prod ON item.prod_id = prod.prod_id
+                                INNER JOIN users AS us ON ord.user_id=us.user_id
+                                WHERE ord.order_id=${order_id}`,
+                    { type: seq.QueryTypes.SELECT }
+                    )
+                    .then((resultados) => {
+                        if (resultados!=""){
+                            res.status(200).send("Listado de pedidos" + JSON.stringify(resultados))
+                        } else{
+                            res.status(404).send("No se encontraron pedidos")
+                        }
+                    })
+                    
+                } else {
+                    //Si es usuario traigo sólo el pedido si corresponde a su id de usuario
+                    seq.query(`SELECT ord.order_id, ord.user_id, ord.payment_id, ord.status_id, ord.order_total, 
+                                item.item_id, item.item_cant, item.item_price,
+                                prod.prod_id, prod.prod_name,
+                                us.address, us.email, us.full_name, us.user_name, us.telephone
+                                FROM order_header AS ord
+                                INNER JOIN order_items AS item ON ord.order_id = item.order_id
+                                INNER JOIN products AS prod ON item.prod_id = prod.prod_id
+                                INNER JOIN users AS us ON ord.user_id=us.user_id
+                                WHERE order_header.user_id = ${id} AND order_header.order_id=${order_id}`,
+                    { type: seq.QueryTypes.SELECT }
+                    )
+                    .then((resultados) => {
+                        if (resultados!=""){
+                            res.status(200).send("Información sobre su pedido" + JSON.stringify(resultados))
+                        } else{
+                            res.status(404).send("Pedido no encontrado o incorrecto")
+                        }
+                    })
+                }
+            });
+    })
+
+    .put(middlewares.autentication, middlewares.isAdmin, (req, res) => {
+        var order_id = req.params.order_id;
+        var status = req.body;
+        seq.query(`SELECT * FROM order_header WHERE order_id=${order_id}`,
+        { type: seq.QueryTypes.SELECT }
+        )
+        .then((resultados) => {
+            //Si el pedido existe, actualizo su estado
+            if (resultados!="") {
+                seq.query(`UPDATE order_header SET status_id = :status_id WHERE order_id=${order_id}`,
+                {replacements: status }
+                ) 
+                .then(resultados => res.status(200).send("El estado del pedido ha sido actualizado"))
+            }else {
+                res.status(404).send("ID de pedido no encontrado")
+            }
         
-        var prod_id = req.body.prod_id;
-        var item_price;
-        seq.query(`SELECT prod_price FROM products WHERE prod_id=${prod_id}`
-        )
-        .then(resultados => {
-            item_price = JSON.stringify(resultados[0])
         })
-        seq.query(`INSERT INTO order_items (order_id, prod_id, item_cant, item_price) 
-                VALUES ('${order}', :prod_id, :item_cant, :item_cant*${item_price}`
-        , { replacements: order }
+    })
+
+    .delete(middlewares.autentication, middlewares.isAdmin, (req, res) => {
+        var order_id = req.params.order_id;
+        seq.query(`SELECT * FROM order_header WHERE order_id=${order_id}`,
+        { type: seq.QueryTypes.SELECT }
         )
-        .then(resultados => {
-        order_id = JSON.stringify(resultados[0])
-        res.redirect(`orders/${order_id}/items`)
-        });
+        .then((resultados) => {
+            //Si el pedido existe, actualizo su estado
+            if (resultados!="") {
+                seq.query(`DELETE FROM order_items WHERE order_id=${order_id}`) 
+                .then(resultados => 
+                    seq.query(`DELETE FROM order_header WHERE order_id=${order_id}`) 
+                    .then(resultados => res.status(200).send("El pedido ha sido eliminado"))
+                )
+            }else {
+                res.status(404).send("ID de pedido no encontrado")
+            }
+        })
     })
 
   module.exports = router;
